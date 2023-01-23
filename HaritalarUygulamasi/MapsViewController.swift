8a5d21d3f5fc3b14e2a723bc62146a253076c5f0
mbaransen <94108242+mbaransen@users.noreply.github.com>
2023-01-23 16:14:35 +0300

    Update MapsViewController.swift

--- HaritalarUygulamasi/MapsViewController.swift
+++ HaritalarUygulamasi/MapsViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Muzaffer Baran on 22.01.2023.
 //
 
+
 import UIKit
 import MapKit //Haritaları kullanacagım icin bu kütüphaneyi eklemem gerek.
 import CoreLocation //Kullanıcının konumunu almak için bu kütüphaneyi eklemem gerek.
@@ -19,6 +20,14 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     var secilenLatitude = Double()
     var secilenLongitude = Double()
     
+    var secilenIsim = ""
+    var secilenId : UUID?
+    
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var anotationLatitude = Double()
+    var annotationLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self //haritamı dogruladım.
@@ -31,6 +40,61 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(konumSec(gestureRecognizer: )))
         gestureRecognizer.minimumPressDuration = 3 //kullanıcı 3 saniye basılı tuttugunda oraya basıldıgını anlaycak
         mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if secilenIsim != "" {
+            //Core Data'dan verileri çek
+            if let uuidString = secilenId?.uuidString {
+                
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "yer")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do {
+                    let sonuclar = try context.fetch(fetchRequest)
+                    
+                    if sonuclar.count > 0 {
+                        for sonuc in sonuclar as! [NSManagedObject]{
+                            if let isim = sonuc.value(forKey: "isim") as? String {
+                                annotationTitle = isim
+                            }
+                            if let not = sonuc.value(forKey: "not") as? String {
+                                annotationSubtitle = not
+                            }
+                            if let latitude = sonuc.value(forKey: "latitude") as? Double{
+                                anotationLatitude = latitude
+                            }
+                            if let longitude = sonuc.value(forKey: "longitude") as? Double {
+                                annotationLongitude = longitude
+                            }
+                            
+                            let annotation = MKPointAnnotation()
+                            annotation.title = annotationTitle
+                            annotation.subtitle = annotationSubtitle
+                            let coordinate = CLLocationCoordinate2D(latitude: anotationLatitude, longitude: annotationLongitude)
+                            annotation.coordinate = coordinate
+                            
+                            mapView.addAnnotation(annotation)
+                            isimTextField.text = annotationTitle
+                            notTextField.text = annotationSubtitle
+                            
+                            locationManager.stopUpdatingLocation()
+                            
+                            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                            let region = MKCoordinateRegion(center: coordinate, span: span)
+                            mapView.setRegion(region, animated: true)
+                        }
+                    }
+                    
+                } catch {
+                    print("hata")
+                }
+            }
+        } else {
+            //yeni veri eklemeye geldi
+        }
     }
     
     @objc func konumSec(gestureRecognizer : UILongPressGestureRecognizer){
@@ -55,11 +119,14 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //print(locations[0].coordinate.latitude)//Haritadaki enlem icin.
         //print(locations[0].coordinate.longitude)//Haritadaki boylam icin.
-        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        if secilenIsim == "" {
+            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+            
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)//Belirttiğin bölgenin yükseklik ve genişliği.Ne kadar küçük seçersen o kadar zoomlu gösterir ilk başta.
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+        }
         
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)//Belirttiğin bölgenin yükseklik ve genişliği.Ne kadar küçük seçersen o kadar zoomlu gösterir ilk başta.
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
     }
 
     @IBAction func kaydetTiklandi(_ sender: Any) {
